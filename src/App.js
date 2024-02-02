e4759b8d8b0a12682bc0c050f5c4b4b656c629df
Kofi Osei-Okyere <rhis.cky10@gmail.com>
2024-02-02 12:48:56 +0000

    class-forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+    };
+  }
+
+  handleNameChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  render() {
+    console.log(this.state.name);
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          onChange={this.handleNameChange}
+          value={this.state.name}
+          type="text"
+          placeholder="Enter your name"
+        />
+        <input
+          onChange={this.handleEmailChange}
+          value={this.state.email}
+          type="email"
+          placeholder="Enter your email"
+        />
+        <input
+          onChange={this.handlePasswordChange}
+          value={this.state.password}
+          type="password"
+          placeholder="Enter your password"
+        />
+        <input type="submit" />
+      </form>
+    );
+  }
 }
 
 export default App;
